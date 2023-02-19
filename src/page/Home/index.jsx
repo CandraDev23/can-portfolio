726570e22b7a8830585d87f234f6d22b68486c86
candradev23 <cand.setiawan23@gmail.com>
2023-02-19 22:45:19 +0700

    adding parallax-star

--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,10 +1,14 @@
 import Body from "../../templates/Body";
 import Footer from "../../templates/Footer";
+import './style.css';
 
 export default function Home() {
     return (
-        <div style={{backgroundColor: "#EFFCFF", padding: "0px"}}>
+        <div style={{ backgroundColor: "#EFFCFF", padding: "0px" }}>
             <Body />
+            <div id="stars"></div>
+            <div id="stars2"></div>
+            <div id="stars3"></div>
             <Footer />
         </div>
     );
